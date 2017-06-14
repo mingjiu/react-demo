36c8bf424228dd3c40bebffe60ed863c4c21d6e0
mjhuang <hmj0718@163.com>
2017-06-14 10:03:59 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+
+import Author from './components/author'
+import Book from './components/book'
+import Category from './components/category'
 
 import './scss/index.scss'
 
-class App extends Component {
-  render() {
+function App () {
     return (
-      <div>Hello world--hot update, no bug asdf</div>
+      <Router>
+        <div>
+          <div>
+            <Link to="/author">作者</Link>
+            <Link to="/category">分类</Link>
+            <Link to="/book">书籍</Link>
+          </div>
+          <div>
+            <Route path="/author" component={Author} />
+            <Route path="/category" component={Category} />
+            <Route path="/book" component={Book} />
+          </div>
+        </div>
+      </Router>
     )
-  }
 }
 
 export default App
